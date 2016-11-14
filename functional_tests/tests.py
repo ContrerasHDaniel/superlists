738526b0bceacdca7bbb12327eedba70d52a44ec
Daniel Contreras <daxez12@gmail.com>
2016-11-13 22:43:01 -0600

    move sqlite database outside of main source tree

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -34,7 +34,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):  
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -122,7 +122,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         def test_layout_and_styling(self):
             # Edith goes to the home page
-            self.browser.get(self.live_server_url)
+            self.browser.get(self.server_url)
             self.browser.set_windows_size(1024, 768)
 
             # She notices the input box is nicely centered
